207ffca7591884621efb2a24c4fa432cb1c4f8c0
Michal <khakaimic4@gmail.com>
2023-03-11 13:12:41 +0300

    completed lab

---
+++ index.js
@@ -0,0 +1,32 @@
+
+function add(a,b){
+    return a+b;
+ }
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+function increment(n){
+    return n+=1;
+}
+function decrement(n){
+    return n-=1;
+}
+
+let number = 7
+function makeInt(number){
+    parseInt("number",10);
+    return number
+}
+function makeInt(number){
+    return parseFloat(number);
+}
+
+function preserveDecimal(number){
+    return parseFloat(number);
+}
